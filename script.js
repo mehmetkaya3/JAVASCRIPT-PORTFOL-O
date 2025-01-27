f8ffffd1ac4e8cbb91b11b2c8468fbb4f127913a
mehmetkaya3 <mehmet.kaya@egeacademy.ca>
2025-01-28 00:51:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('.buttons a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
